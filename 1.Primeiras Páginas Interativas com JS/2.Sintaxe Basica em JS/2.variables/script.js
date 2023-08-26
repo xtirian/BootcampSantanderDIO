ec32810fa7c90c2382b3b82888e6766a904d2f3a
xTirian <tirianx@yahoo.com>
2023-08-25 22:36:23 -0300

    aula de variables

--- 1.Primeiras Páginas Interativas com JS/2.Sintaxe Basica em JS/2.variables/script.js
+++ 1.Primeiras Páginas Interativas com JS/2.Sintaxe Basica em JS/2.variables/script.js
@@ -1,3 +1,20 @@
+const { text } = require("express")
+
 var vOuF = false
 
 console.log(vOuF)
+
+console.log(typeof(vOuF))
+
+var numero = 1
+
+console.log(typeof(numero))
+
+
+var texto = "um texto qualquer"
+
+console.log(texto)
+
+console.log(typeof(texto))
+
+
