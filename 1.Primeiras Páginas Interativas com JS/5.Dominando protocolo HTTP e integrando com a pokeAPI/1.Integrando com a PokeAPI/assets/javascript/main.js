5f094bfc9888facc1db01946999fe77687ed5c00
xTirian <tirianx@yahoo.com>
2023-08-31 23:45:12 -0300

    finish the api usage, next step is create a button for the next page and previous page

--- 1.Primeiras Páginas Interativas com JS/5.Dominando protocolo HTTP e integrando com a pokeAPI/1.Integrando com a PokeAPI/assets/javascript/main.js
+++ 1.Primeiras Páginas Interativas com JS/5.Dominando protocolo HTTP e integrando com a pokeAPI/1.Integrando com a PokeAPI/assets/javascript/main.js
@@ -8,12 +8,64 @@ if (window.innerWidth <= 586) {
   pageCardLimit = 25;
 }
 
+
+
 function createCard(pokemon) {
-  return `<li class="lista-pokemons-card" style="background-image: url(./assets/imagens/1.svg); ">
-      <span class="lista-pokemons-id">#001</span>
-      <h2 class="lista-pokemons-card-name">${pokemon.name}</h2>
-      <p class="lista-pokemons-card-info">Grass</p>
-      <p class="lista-pokemons-card-info">Poison</p>
+  let name = pokemon.name.split("");
+  let id = String(pokemon.id).split("");
+  let img = pokemon.sprites.other.dream_world.front_default;
+  let type1 = pokemon.types[0].type.name.split("");
+  let type2 = [" "];  
+
+  //Set Uppercase in the first letter of the name
+  // let firstLetter = name[0].toUpperCase();
+  // name.shift();
+  // name.unshift(firstLetter);
+  // name = name.join("");
+  
+
+  //add the 0 before the ID
+  while (id.length <= 2) {
+    id.unshift("0");
+  }
+
+  id = id.join("");
+
+  //set the type 2
+
+  if (pokemon.types[1] != undefined) {
+    type2 = pokemon.types[1].type.name.split("");
+  }
+
+  
+  
+
+  //set UpperCase to the first letter os each type
+
+  let datas = [name, type1, type2];
+
+  datas.forEach((info) => {
+    let firstLetter = info[0].toUpperCase();
+    info.shift();
+    info.unshift(firstLetter);
+  });
+
+  //define if there is second type in the HTML
+  let setType2 = ""; 
+
+  console.log(type2[0])
+  if(type2[0] != [" "]){    
+    setType2 = `<p class="lista-pokemons-card-info">${datas[2].join("")}</p>`
+  } 
+
+
+
+  return `<li class="lista-pokemons-card type type-${datas[1].join("")}" style="background-image: url(${img}); ">
+      <span class="lista-pokemons-id">#${id}</span>
+      <h2 class="lista-pokemons-card-name">${datas[0].join("")}</h2>
+      <p class="lista-pokemons-card-info">${datas[1].join("")}</p>
+      ${setType2}
+      
      </li>`;
 }
 
@@ -25,23 +77,36 @@ const putCardOnScreen = (result) => {
   Page.innerHTML = result.map(createCard).join("");
 };
 
-function Fetch() {
-  fetch(url)
-    .then((response) => response.json())
-    .then((data) => {
-      return {
-        pageInfo: data,
-        results: data.results,
-      };
-    })
-    .then((obj) => {
-      let result = obj.results;
-      let pageInfo = obj.pageInfo;
+async function getPokemonDetail(pokemon) {
+  let response = await fetch(pokemon.url);
+  return await response.json();
 
-      putCardOnScreen(result);     
-      
-    })
-    .catch((error) => console.error(error));
+  // return await fetch(pokemon.url).then((response) => response.json());
 }
 
-Fetch();
+async function Fetch() {
+  let response = await fetch(url);
+  let data = await response.json();
+
+  let firstResults = await data.results;
+
+  let newResults = [];
+
+  for (let x = 0; x < firstResults.length; x++) {
+    await getPokemonDetail(firstResults[x]).then((pokeObj) =>
+      newResults.push(pokeObj)
+    );
+  }
+
+  console.log(newResults[0]);
+
+  // let newResults = await firstResults.map(getPokemonDetail);
+
+  return newResults;
+}
+
+Fetch().then((pokeDetails) => {
+  let Page = document.getElementById("pokedex-page");
+
+  Page.innerHTML = pokeDetails.map(createCard).join("");
+});
