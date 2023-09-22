8029c8340970de7898e53b0f824cf54b3ecc49d7
xTirian <tirianx@yahoo.com>
2023-09-22 19:36:50 -0300

    fix: folders

--- 3.Angular/projeto1-angular/src/app/app.component.spec.ts
+++ 3.Angular/projeto1-angular/src/app/app.component.spec.ts
@@ -14,16 +14,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'primeiro-projeto'`, () => {
+  it(`should have as title 'projeto1-angular'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('primeiro-projeto');
+    expect(app.title).toEqual('projeto1-angular');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('primeiro-projeto app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('projeto1-angular app is running!');
   });
 });
